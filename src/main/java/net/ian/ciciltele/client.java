75a11a32ace2a84a222b7f5d2f02771ec063d4ee
ianke <iankengott38@gmail.com>
2025-02-05 18:10:06 -0500

    first

---
+++ src/main/java/net/ian/ciciltele/client.java
@@ -0,0 +1,10 @@
+package net.ian.ciciltele;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
