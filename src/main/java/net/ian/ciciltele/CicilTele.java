272b8601e32f76021378a6e3766e135679b22e3c
ianke <iankengott38@gmail.com>
2025-02-05 18:22:38 -0500

    important

--- src/main/java/net/ian/ciciltele/CicilTele.java
+++ src/main/java/net/ian/ciciltele/CicilTele.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//hi
 public class CicilTele implements ModInitializer {
 	public static final String MOD_ID = "ciciltele";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
